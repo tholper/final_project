d20547fee4125c9d838e0cbc9912fda2df086f12
Raghu Betina <raghu@firstdraft.com>
2018-11-27 21:24:59 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "publications#index"
   # Routes for the Publication resource:
 
