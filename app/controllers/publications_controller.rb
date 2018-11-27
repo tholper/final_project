59a8cc9b0b7360f73d7db6af9bad149c2e801d2b
Raghu Betina <raghu@firstdraft.com>
2018-11-27 21:25:06 +0000

    Only publication user can modify publication

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,4 +1,14 @@
 class PublicationsController < ApplicationController
+  before_action :current_user_must_be_publication_publication, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_publication_publication
+    publication = Publication.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == publication.publication
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @publications = Publication.all
 
